3566fd558d7903ca4c8a98f55b9434556ee3dcbe
Muhammadsolih <solix200@gmail.com>
2023-05-11 13:12:18 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -113,8 +113,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = (Path(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
